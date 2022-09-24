995df4d525db8357611c5c9cc642d0551f33501c
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-09-24 12:31:23 -0700

    hauuugh2

--- readCsv.py
+++ readCsv.py
@@ -4,9 +4,16 @@ import numpy as np
 
 def readCsv(csv):
     csRead = pd.read_csv(os.getcwd() + "/" + csv)
+    return csRead
    
 
-def toHotEncodes(csv):
-    hotEncode = pd.get_dummies(csv)
-    return np.array(hotEncode)
-    #print(hotEncode.head())
+def toHotEncodes(csv, output):
+    try:
+        df = readCsv(csv)
+        hotEncode = pd.get_dummies(df[output])
+        return np.array(hotEncode)
+        #print(hotEncode.head())
+    except:
+        raise ValueError("bruh u need to actually have that")
+
+#print(toHotEncodes("iris.csv", "variety"))
