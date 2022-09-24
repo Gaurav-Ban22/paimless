e65a1360bf5b84cb0cf0addb8503f2efca285935
Andrew Fan <andrew@LAPTOP-ES22F19F.localdomain>
2022-09-24 12:55:47 -0700

    janknkshuehuehuehehuheuhh

--- csvprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-
-def process_csv_onehot(path, input_cols, output_col):
-    df = pd.read_csv(path)
-    y_train = df.columns[output_col]
-    print(y_train)
-
-    
