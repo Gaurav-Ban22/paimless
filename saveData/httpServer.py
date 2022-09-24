fa9f2f0fcf842d91f12a03b9e45f09f252eba947
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-09-24 15:32:38 -0700

    better server

--- saveData/httpServer.py
+++ saveData/httpServer.py
@@ -3,10 +3,35 @@ from socket import socket
 import socketserver
 
 
-PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
+class httpServer:
+    def __init__(self, port=8080):
+        self.port = port
+        self.server = None
+        self.handler = None
+
+    def start(self):
+        self.handler = http.server.SimpleHTTPRequestHandler
+        self.server = socketserver.TCPServer(("", self.port), self.handler)
+        self.server.serve_forever()
+
+    def stop(self):
+        self.server.shutdown()
+        self.server.socket.close()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        print(self.wfile)
+        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body><p>This is a test.</p></body></html>", "utf-8"))
+        self.wfile.close()
 
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("hosting on", PORT)
-    httpd.serve_forever()
+try:
+    serv = httpServer(8000)
+    serv.start()
+except KeyboardInterrupt:
+    print(" Keyboard interrupt; stopping server")
+    serv.stop()
+    
